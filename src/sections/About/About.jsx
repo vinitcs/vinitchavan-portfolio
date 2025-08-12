e2b05be4a3f01963dc92047789580ff4c449f019
vinitcs <vinitchavan1234@gmail.com>
2025-08-12 10:20:20 +0530

    updt: resume update

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,7 +1,7 @@
 import styles from "./AboutStyles.module.css";
 import Paragraph from "../../components/Paragraph/Paragraph";
 
-const paragraph = "Versatile developer skilled in building user-centered web and mobile applications using React.js, React Native, and the MERN stack. Adept at creating dynamic, scalable solutions that enhance performance and interactivity. Seeking a full-time role to drive innovation and contribute to impactful projects while advancing my expertise.";
+const paragraph = "MERN developer skilled in building web and mobile applications using React.js, React Native, Node.js, and MongoDB. Adept at creating dynamic, scalable solutions that enhance performance and interactivity, while following industry standards for developing optimized APIs. Passionate about contributing to impactful projects while advancing my expertise.";
 
 function About() {
      return (
